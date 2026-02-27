7de8d36f92adaa0a9bf064a548b14be9788b49ba
KevinPaez28 <Kevinandreypaezgarces@gmail.com>
2026-02-27 14:36:06 -0500

    se implementa modificaciones en vite.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  publicDir: false,
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: assetInfo => {
-          const { name } = assetInfo
-          if (name && name.startsWith('views/')) {
-            return name
-          }
-          return 'assets/[name]-[hash][extname]'
-        }
-      }
-    }
-  }
+  publicDir: 'src/views'  // Copia src/views/ → dist/views/
 })
